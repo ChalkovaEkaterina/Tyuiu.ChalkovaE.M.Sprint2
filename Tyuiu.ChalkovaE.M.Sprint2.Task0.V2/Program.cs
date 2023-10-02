55d4ade0f7eb130e815ddf49dde1a5ed10568e45
Tyuiu.ChalkovaEM@outlook.com <tch4cat@yandex.ru>
2023-10-02 22:31:58 +0500

    Add project files.

---
+++ Tyuiu.ChalkovaE.M.Sprint2.Task0.V2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ChalkovaE.M.Sprint2.Task0.V2.Lib;
+
+namespace Tyuiu.ChalkovaE.M.Sprint2.Task0.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = 123;
+            int y = 123;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            
+            Console.Title = "Спринт #2| Выполнила: Чалкова Е. М. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                 ");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнила Чалкова Е. М. | ИИПб-23-2                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений и арифметических выражений,    *");
+            Console.WriteLine("* которая вернет логическую последовательность:                           *");
+            Console.WriteLine("* (False, True, False, True, False, True), при x = 123, y = 123.          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
