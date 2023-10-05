c9fc53595d3d8b600e640d71818b275ffc1f9866
Tyuiu.ChalkovaEM@outlook.com <tch4cat@yandex.ru>
2023-10-05 17:30:27 +0500

    Task3

---
+++ Tyuiu.ChalkovaE.M.Sprint2.Task3.V16.Lib/DataService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ChalkovaE.M.Sprint2.Task3.V16.Lib
+{
+    public class DataService : ISprint2Task3V16
+    {
+        public double Calculate(double x)
+        {
+            double y = 0;
+
+            if (x > 0)
+            {
+                y = x + Math.Pow(((x + 1) / (x - 1)), 3);
+            }
+            else
+            {
+                if (x == 0)
+                {
+                    y = (2 * Math.Pow(x, 2) - Math.Cos(Math.Pow(x, 2)) + 10) / (5 * Math.Pow(x, 2) - Math.Sin(Math.Pow(x, 2)) + 12);
+                }
+                else
+                {
+                    if ((x > -20) && (x < 0))
+                    {
+                        y = Math.Pow(1 + (1 / Math.Pow(x, 2)), 7);
+                    }
+                    else
+                    {
+                        if (x < -20)
+                        {
+                            y = x + 10 * x - (1 / x);
+                        }
+                    }
+                }
+            }
+            return Math.Round(y, 3);
+        }
+    }
+}
---
+++ Tyuiu.ChalkovaE.M.Sprint2.Task3.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ChalkovaE.M.Sprint2.Task3.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ChalkovaE.M.Sprint2.Task3.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3d26133b-aba5-43f7-bac0-c16505033a73")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
