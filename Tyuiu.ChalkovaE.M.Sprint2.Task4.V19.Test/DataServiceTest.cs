52ca9d8659449fc7be719ec29253432db67dc646
Tyuiu.ChalkovaEM@outlook.com <tch4cat@yandex.ru>
2023-10-08 19:54:33 +0500

    Task4

---
+++ Tyuiu.ChalkovaE.M.Sprint2.Task4.V19.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ChalkovaE.M.Sprint2.Task4.V19.Lib;
+
+namespace Tyuiu.ChalkovaE.M.Sprint2.Task4.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 10;
+            double res = ds.Calculate(x, y);
+            double wait = 36.482;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = 1.115;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
