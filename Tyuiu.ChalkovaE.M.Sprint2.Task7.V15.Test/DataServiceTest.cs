5d3a04ab8d4be8ab0e4f57a6f2aa798d5e003641
Tyuiu.ChalkovaEM@outlook.com <tch4cat@yandex.ru>
2023-10-13 22:25:01 +0500

    Task7

---
+++ Tyuiu.ChalkovaE.M.Sprint2.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ChalkovaE.M.Sprint2.Task7.V15.Lib;
+
+namespace Tyuiu.ChalkovaE.M.Sprint2.Task7.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Valid()
+        {
+            DataService ds = new DataService();
+            double x = -0.1;
+            double y = 1.5;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
